3d3da0ab8f5c51ca250c5cd919845edde3454d0b
Rafael Alexandro Vuelvas Pérez <138848815+ELNARMOTON79@users.noreply.github.com>
2025-03-15 12:07:05 -0600

    Codigo optimizado para raspberry

--- configuracion.py
+++ configuracion.py
@@ -1,6 +1,6 @@
 from customtkinter import *
 
-class Configuracion(CTkFrame ):
+class Configuracion(CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         
@@ -8,5 +8,6 @@ class Configuracion(CTkFrame ):
         self.main_frame = CTkFrame(self, fg_color="#CFF4D2")  # Fondo verde claro
         self.main_frame.pack(fill="both", expand=True, padx=10, pady=10)
 
+        # Título "Settings"
         self.title_label = CTkLabel(self.main_frame, text="Settings", font=("Arial", 20, "bold"), text_color="#32909C")
         self.title_label.pack(pady=(10, 5))  # Espaciado superior
